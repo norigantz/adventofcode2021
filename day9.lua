492f459f7688b902a12ae9f48efe4de955267f30
Miles Raphael <norigantz@gmail.com>
2021-12-09 19:14:45 -0500

    day9b

--- day9.lua
+++ day9.lua
@@ -14,10 +14,61 @@ end
 function day9.b(arr)
     local heightmap = day9.build_heightmap(arr)
     local low_points = day9.find_low_points(heightmap)
-
+    local basins = {} -- basins[b], key corresponds to its low_point, value is size
     for a,b in pairs(low_points) do
-        
+        if basins[a] == nil then basins[a] = 0 end
+        local x, y = b[1], b[2]
+        local visited = {}
+        local search = { b }
+        repeat
+            local next = table.remove(search, #search)
+            x, y = next[1], next[2]
+            table.insert(visited, next)
+            basins[a] = basins[a] + 1
+            if heightmap[x-1] ~= nil and tonumber(heightmap[x-1][y]) < 9 then
+                for va,vb in pairs(visited) do
+                    if x-1 == vb[1] and y == vb[2] then goto continue end
+                end
+                for sa,sb in pairs(search) do
+                    if x-1 == sb[1] and y == sb[2] then goto continue end
+                end
+                table.insert(search, { x-1, y })
+                ::continue::
+            end
+            if heightmap[x+1] ~= nil and tonumber(heightmap[x+1][y]) < 9 then
+                for va,vb in pairs(visited) do
+                    if x+1 == vb[1] and y == vb[2] then goto continue end
+                end
+                for sa,sb in pairs(search) do
+                    if x+1 == sb[1] and y == sb[2] then goto continue end
+                end
+                table.insert(search, { x+1, y })
+                ::continue::
+            end
+            if heightmap[x][y-1] ~= nil and tonumber(heightmap[x][y-1]) < 9 then
+                for va,vb in pairs(visited) do
+                    if x == vb[1] and y-1 == vb[2] then goto continue end
+                end
+                for sa,sb in pairs(search) do
+                    if x == sb[1] and y-1 == sb[2] then goto continue end
+                end
+                table.insert(search, { x, y-1 })
+                ::continue::
+            end
+            if heightmap[x][y+1] ~= nil and tonumber(heightmap[x][y+1]) < 9 then
+                for va,vb in pairs(visited) do
+                    if x == vb[1] and y+1 == vb[2] then goto continue end
+                end
+                for sa,sb in pairs(search) do
+                    if x == sb[1] and y+1 == sb[2] then goto continue end
+                end
+                table.insert(search, { x, y+1 })
+                ::continue::
+            end
+        until #search == 0
     end
+    table.sort(basins)
+    print(tonumber(basins[#basins]) * tonumber(basins[#basins-1]) * tonumber(basins[#basins-2]))
 end
 
 function day9.find_low_points(heightmap)
@@ -31,10 +82,7 @@ function day9.find_low_points(heightmap)
                heightmap[x][y+1] ~= nil and heightmap[x][y+1] <= curr then
                 goto continue
             end
-            local pair = {}
-            table.insert(pair, x)
-            table.insert(pair, y)
-            table.insert(low_points,pair)
+            table.insert(low_points, { x, y })
             ::continue::
         end
     end
