18dab0d3036cd9f8cef699c7e8b103b429e79c6b
Miles Raphael <norigantz@gmail.com>
2021-12-14 20:18:08 -0500

    day14

--- day14.lua
+++ day14.lua
@@ -1,69 +1,89 @@
 day14 = {}
 
-local counts = {}
-
 function day14.a(arr)
-    local polymer = arr[1]
-    day14.init_counts(polymer)
-    local insertion_rules = day14.build_insertions(arr)
-    local steps = 10
-    for i=1,steps do
-        polymer = day14.insert_polymers(polymer, insertion_rules)
-        print(counts["H"])
-    end
-    print(day14.count_result(polymer))
+    print(day14.solve(arr, 10))
 end
 
 function day14.b(arr)
-    local polymer = arr[1]
-    day14.init_counts(polymer)
-    local insertion_rules = day14.build_insertions(arr)
-    local steps = 40
-    for i=1,steps do
-        polymer = day14.insert_polymers(polymer, insertion_rules)
-    end
-    print(day14.count_result(polymer))
+    print(day14.solve(arr, 40))
 end
 
-function day14.count_result(polymer)
-    local count_array = {}
-    for a,b in pairs(counts) do
-        table.insert(count_array, b)
-    end
-    table.sort(count_array)
-    return count_array[#count_array] - count_array[1]
-end
+function day14.solve(arr, steps)
+    local pair_counts = {}
+    local rules = {}
 
-function day14.insert_polymers(polymer, rules)
-    for i=#polymer-1,1,-1 do
-        local curr_pair = polymer:sub(i,i+1)
-        if rules[curr_pair] ~= nil then
-            polymer = polymer:sub(1,i)..rules[curr_pair]..polymer:sub(i+1)
-            if counts[rules[curr_pair]] == nil then counts[rules[curr_pair]] = 1
-            else counts[rules[curr_pair]] = counts[rules[curr_pair]] + 1 end
-        end
-    end
-    return polymer
-end
-
-function day14.build_insertions(arr)
-    local insertion_rules = {}
+    -- Init pair_counts to 0 and insertion rules lookup
     for i=3,#arr do
         local rule = {}
         for s in string.gmatch(arr[i], "%a+") do
             table.insert(rule, s)
         end
-        insertion_rules[rule[1]] = rule[2]
+        pair_counts[rule[1]] = 0
+        rules[rule[1]] = rule[2]
     end
-    return insertion_rules
-end
 
-function day14.init_counts(polymer)
-    for i=1,#polymer do
-        local char = polymer:sub(i,i)
-        if counts[char] == nil then counts[char] = 1
-        else counts[char] = counts[char] + 1 end
+    -- Init pair_counts with initial polymer string
+    local polymer = arr[1]
+    for i=1,#polymer-1 do
+        pair_counts[polymer:sub(i,i+1)] = pair_counts[polymer:sub(i,i+1)] + 1
     end
+
+    -- Run insertion rules
+    local step = steps
+    for i=1,step do
+        -- Init next pair count table to 0
+        local next_pair_counts = {}
+        for i=3,#arr do
+            local rule = {}
+            for s in string.gmatch(arr[i], "%a+") do
+                table.insert(rule, s)
+            end
+            next_pair_counts[rule[1]] = 0
+        end
+
+        -- Increment next pair counts by current pair counts based on rules
+        for a,b in pairs(pair_counts) do
+            if b > 0 then
+                next_pair_counts[a:sub(1,1)..rules[a]] = next_pair_counts[a:sub(1,1)..rules[a]] + b
+                next_pair_counts[rules[a]..a:sub(2,2)] = next_pair_counts[rules[a]..a:sub(2,2)] + b
+            end
+        end
+
+        -- Set pair counts to next pair counts
+        for a,b in pairs(pair_counts) do
+            pair_counts[a] = next_pair_counts[a]
+        end
+    end
+
+    -- Counting chars
+    local char_counts = {}
+    for a,b in pairs(pair_counts) do
+        for i=1,2 do
+            local char = a:sub(i,i)
+            if char_counts[char] == nil then
+                char_counts[char] = b
+            else
+                char_counts[char] = char_counts[char] + b
+            end
+        end
+    end
+
+    -- Since each char appears in two pairs, except the edges: dividing by two scheme
+    for a,b in pairs(char_counts) do
+        if a == polymer:sub(1,1) or a == polymer:sub(#polymer,#polymer) then
+            char_counts[a] = char_counts[a] + 1
+        end
+        char_counts[a] = char_counts[a] // 2
+    end
+
+    -- Sorting
+    local char_counts_arr = {}
+    for a,b in pairs(char_counts) do
+        table.insert(char_counts_arr, b)
+    end
+    table.sort(char_counts_arr)
+
+    return char_counts_arr[#char_counts_arr] - char_counts_arr[1]
 end
 
 return day14
